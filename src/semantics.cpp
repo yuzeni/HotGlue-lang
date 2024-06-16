fc06338c5906a3a605ef9a8179e98de4c0c4ecfa
Yunus Zenichowski <y.zenichowski@gmail.com>
2024-06-16 11:48:00 +0200

    base type compare bug fix

--- src/semantics.cpp
+++ src/semantics.cpp
@@ -28,13 +28,13 @@ consteval std::array<Type_compare, T_SIZE * T_SIZE> get_type_compare_table()
 		table[TC_TABLE_IDX(type_a, type_b)] = Type_compare::Equal;
 	    else if(Type_num_limits_table[type_a].max < Type_num_limits_table[type_b].max
 		    && Type_num_limits_table[type_a].min >= Type_num_limits_table[type_b].min
-		    && !is_base_type_floating_point(Type_enum(type_a)))
+		    && !(is_base_type_floating_point(Type_enum(type_a)) && is_base_type_integer(Type_enum(type_b))))
 	    {
 		table[TC_TABLE_IDX(type_a, type_b)] = Type_compare::A_subset_B;
 	    }
 	    else if(Type_num_limits_table[type_a].max > Type_num_limits_table[type_b].max
 		    && Type_num_limits_table[type_a].min <= Type_num_limits_table[type_b].min
-		    && !is_base_type_floating_point(Type_enum(type_b)))
+		    && !(is_base_type_floating_point(Type_enum(type_b)) && is_base_type_integer(Type_enum(type_a))))
 	    {
 		table[TC_TABLE_IDX(type_a, type_b)] = Type_compare::B_subset_A;
 	    }
